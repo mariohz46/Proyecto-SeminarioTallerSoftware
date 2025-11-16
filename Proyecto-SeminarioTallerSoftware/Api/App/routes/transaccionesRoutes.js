0af1422e4ffea87c72a977f3dd764295d04b8153
AnaCord <anag_cordovaa@unicah.edu>
2025-11-16 15:21:52 -0600

    Revert "Agrega finanzas-frontend como submódulo"
    This reverts commit 8618d8a85d1c1e883c556e1c131eaa1c41942b70.

--- Proyecto-SeminarioTallerSoftware/Api/App/routes/transaccionesRoutes.js
+++
@@ -1,6 +0,0 @@
-const express =require('express');
-const router= express.Router();
-const TransaccionesController =require('../controllers/transaccionesController');
-const {autenticarToken} =require('../services/authTokenServices');
-
-router.post('/nuevaTransaccion',autenticarToken,TransaccionesController.crearTransaccion);
--- Proyecto-SeminarioTallerSoftware/Api/App/routes/usuariosRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router =express.Router();
-const usuariosController = require('../controllers/usuariosController');
-
-router.post('/registro',usuariosController.registrar);
-router.post('/login',usuariosController.login);
-module.exports =router;
