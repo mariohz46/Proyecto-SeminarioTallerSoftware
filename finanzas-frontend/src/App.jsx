5a42331f7f9995c6985c89b984d6ef1953810748
AnaCord <anag_cordovaa@unicah.edu>
2025-11-23 10:53:37 -0600

    Implementacion del Login

--- finanzas-frontend/src/App.jsx
+++ finanzas-frontend/src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
       <Navbar />
       <main>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Register />} />
           <Route path="/transacciones/nueva" element={<NewTransaction />} />
